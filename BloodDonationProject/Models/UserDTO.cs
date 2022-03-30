8868b6aea0dc3c8a183bed5acea8ffe6beebef87
Amanda Puttomatti <acsputtomatti@gmail.com>
2022-03-30 18:22:07 -0400

    Added POST, PUT and DELETE for Users, Hospital and Donations

--- BloodDonationProject/Models/UserDTO.cs
+++ BloodDonationProject/Models/UserDTO.cs
@@ -10,19 +10,28 @@ namespace BloodDonationProject.Models
     {
         [Required]
         public string firstName { get; set; }
+
         [Required]
         public string lastName { get; set; }
+
         [Required]
-        [StringLength(maximumLength: 6, MinimumLength = 4, ErrorMessage = "Enter 'Male' or 'Female'")]
         public string sex { get; set; }
+
         [Required]
-        [StringLength(maximumLength: 3, ErrorMessage = "Enter correct age")]
         public int age { get; set; }
+
         [Required]
         public string phone { get; set; }
+
         [Required]
         public string address { get; set; }
     }
+
+    public class UpdateUserDTO : CreateUserDTO
+    {
+
+    }
+
     public class UserDTO : CreateUserDTO
     {
         public int Id { get; set; }
